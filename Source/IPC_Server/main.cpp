18ade736dcb31dbfaf892a2f3ae311c221c70c58
SwenenzY <ykaanbayat@gmail.com>
2021-05-15 07:26:48 +0300

    autoCommit 2021 0Sa. 72648

--- Source/IPC_Server/main.cpp
+++ Source/IPC_Server/main.cpp
@@ -1,6 +1,17 @@
 #include "stdafx.h"
 
+
 int main(int argc, char* argv[])
 {
+    #pragma region Clean
+
+    WSACleanup();
+    WSADATA wsa_data;
+    WSAStartup(MAKEWORD(2, 2), &wsa_data);
+
+    #pragma endregion
+
     
+
+    return 0;
 }
