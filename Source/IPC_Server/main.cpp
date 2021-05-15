65e492eb9b2c6492aea6c9bb36d68342457ac4ed
SwenenzY <ykaanbayat@gmail.com>
2021-05-15 07:33:39 +0300

    AutoCommit

--- Source/IPC_Server/main.cpp
+++ Source/IPC_Server/main.cpp
@@ -1,5 +1,6 @@
 #include "stdafx.h"
 
+#define CommPort 3152
 
 int main(int argc, char* argv[])
 {
@@ -11,7 +12,9 @@ int main(int argc, char* argv[])
 
     #pragma endregion
 
+    struct sockaddr_in Server;
     SOCKET Socket;
+
     if ((Socket = socket(AF_INET, SOCK_DGRAM, 0)) == -1) {
         std::cout << "[-] Failed to create socket" << std::endl;
         system("pause");
