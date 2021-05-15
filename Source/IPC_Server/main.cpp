c2f7f4f0865d57a8ca5c57fa8944ff5edbd1fcd9
SwenenzY <ykaanbayat@gmail.com>
2021-05-15 07:36:51 +0300

    AutoCommit

--- Source/IPC_Server/main.cpp
+++ Source/IPC_Server/main.cpp
@@ -2,6 +2,10 @@
 
 #define CommPort 3152
 
+void Listener() {
+
+}
+
 int main(int argc, char* argv[])
 {
     #pragma region Clean
@@ -26,8 +30,10 @@ int main(int argc, char* argv[])
     Server.sin_port = CommPort;
 
     if (bind(Socket, (struct sockaddr*)&Server, sizeof(Server)) == SOCKET_ERROR) {
-        std::cout << "[-] Failed to bind socket : " << std::endl;
+        std::cout << "[-] Failed to bind socket : " << WSAGetLastError() << std::endl;
     }
+
+
 
     std::cout << "[+] Ready " << std::endl;
     return 0;
