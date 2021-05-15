4b60d89405983bda7c30372e0752190d6ec6c87a
SwenenzY <ykaanbayat@gmail.com>
2021-05-15 07:54:21 +0300

    AutoCommit 05/15/2021 7:54:21.23

--- Source/IPC_Client/main.cpp
+++ Source/IPC_Client/main.cpp
@@ -1,5 +1,7 @@
 #include "stdafx.h"
 
+#define CommPort 3152
+
 int main(int argc, char* argv[])
 {
     #pragma region Clean
@@ -12,7 +14,8 @@ int main(int argc, char* argv[])
 
     struct sockaddr_in Server;
     Server.sin_addr.s_addr = INADDR_ANY;
-
+    Server.sin_family = AF_INET;
+    Server.sin_port = CommPort;
 
 
     std::cout << "[+]" << std::endl;
