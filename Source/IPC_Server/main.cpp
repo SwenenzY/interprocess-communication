5131c2a4ac1189283181e5c46ddd85be611a01fd
SwenenzY <ykaanbayat@gmail.com>
2021-05-15 07:32:35 +0300

    AutoCommit

--- Source/IPC_Server/main.cpp
+++ Source/IPC_Server/main.cpp
@@ -12,14 +12,13 @@ int main(int argc, char* argv[])
     #pragma endregion
 
     SOCKET Socket;
-
-    Socket = socket(AF_INET, SOCK_DGRAM, 0);
-
-    if (Socket == -1) {
+    if ((Socket = socket(AF_INET, SOCK_DGRAM, 0)) == -1) {
         std::cout << "[-] Failed to create socket" << std::endl;
         system("pause");
         return 1;
     }
+
+
 
     return 0;
 }
