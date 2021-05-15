99120ab2d9a5dce491bfcbaf2118794941069859
SwenenzY <ykaanbayat@gmail.com>
2021-05-15 07:44:50 +0300

    AutoCommit

--- Source/IPC_Server/main.cpp
+++ Source/IPC_Server/main.cpp
@@ -14,9 +14,9 @@ void Listener() {
             std::cout << "[-] Failed to create recvfrom " << WSAGetLastError() << std::endl;
         }
 
-        std::cout << "[!] Recevied packet from " << inet_ntoa(Listen.sin_addr) << ":" << ntohs(Listen.sin_port) << std::endl;
-
-
+        if (Buffer != '\0') {
+            std::cout << "[!] Recevied packet from " << inet_ntoa(Listen.sin_addr) << ":" << ntohs(Listen.sin_port) << std::endl;
+        }
     }
 }
 
