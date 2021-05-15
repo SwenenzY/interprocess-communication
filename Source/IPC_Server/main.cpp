6835c0e1b393aaecb57e3f6163b7396c85f822fc
SwenenzY <ykaanbayat@gmail.com>
2021-05-15 07:46:27 +0300

    AutoCommit

--- Source/IPC_Server/main.cpp
+++ Source/IPC_Server/main.cpp
@@ -9,7 +9,7 @@ int RecvLen;
 
 void Listener() {
     while (true) {
-        Buffer = {};
+        memset(Buffer, '', 512);
         if ((RecvLen == recvfrom(Socket, Buffer, 512, 0, (struct sockaddr*)&Listen, &FromLen)) == SOCKET_ERROR) {
             std::cout << "[-] Failed to create recvfrom " << WSAGetLastError() << std::endl;
         }
