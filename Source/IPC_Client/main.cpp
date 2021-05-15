c9307ca609dfe6bd2ad41328aa22d961997e3fe3
SwenenzY <ykaanbayat@gmail.com>
2021-05-15 07:54:54 +0300

    AutoCommit 05/15/2021 7:54:54.19

--- Source/IPC_Client/main.cpp
+++ Source/IPC_Client/main.cpp
@@ -12,10 +12,22 @@ int main(int argc, char* argv[])
 
     #pragma endregion
 
+    SOCKET Socket;
     struct sockaddr_in Server;
     Server.sin_addr.s_addr = INADDR_ANY;
     Server.sin_family = AF_INET;
     Server.sin_port = CommPort;
+
+    if ((Socket = socket(AF_INET, SOCK_DGRAM, 0)) == -1) {
+        std::cout << "[-] Failed to create socket" << std::endl;
+        system("pause");
+        return 1;
+    }   
+    if (connect(Socket, (struct sockaddr*)&Server, sizeof(Server)) < 0) {
+        std::cout << "[-] Connection error" << std::endl;
+        system("pause");
+        return 1;
+    }
 
 
     std::cout << "[+]" << std::endl;
